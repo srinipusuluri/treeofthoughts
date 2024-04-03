f26589b0c00b32a642cbd665f02e5dfc39d1c801
ricardo-larosa <ricardo@larosa.dev>
2024-04-03 14:30:45 +0200

    Add initial SWETask script

--- tasks/swe-bench.py
+++ tasks/swe-bench.py
@@ -1,19 +1,25 @@
 import argparse
 from tot.methods.bfs import solve
 from tot.tasks.swe import SWETask
+from datasets import load_dataset
 
-args = argparse.Namespace(backend='gpt-4', 
-                          temperature=0.7, 
-                          task='swe', 
-                          naive_run=False, 
-                          prompt_sample=None, 
-                          method_generate='propose', 
-                          method_evaluate='vote', 
-                          method_select='greedy', 
-                          n_generate_sample=1, 
-                          n_evaluate_sample=3, 
-                          n_select_sample=5)
+train_dataset = load_dataset("princeton-nlp/SWE-bench_Lite", split = "test")
 
-task = SWETask()
-ys, infos = solve(args=args, task=task, idx=1)
+args = argparse.Namespace(
+    backend='gpt-4', 
+    temperature=0.1, 
+    task='swe', 
+    naive_run=False, 
+    prompt_sample=None, 
+    method_generate='propose', 
+    method_evaluate='vote', 
+    method_select='greedy', 
+    n_generate_sample=1, 
+    n_evaluate_sample=3, 
+    n_select_sample=5)
+
+task = SWETask(train_dataset)
+
+ys, infos = solve(args, task, 1)
 print(ys[0])
+    
